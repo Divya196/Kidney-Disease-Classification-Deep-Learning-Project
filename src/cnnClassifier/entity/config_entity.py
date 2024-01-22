d57a95247d20565fb6846ce4c331be015e0400c3
Divya196 <divyaramesh196@gmail.com>
2024-01-22 19:16:51 +0530

    data ingestion added

---
+++ src/cnnClassifier/entity/config_entity.py
@@ -0,0 +1,11 @@
+#Created Custom entity using dataclass
+from dataclasses import dataclass
+from pathlib import Path
+
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+    root_dir: Path
+    source_URL: str
+    local_data_file: Path
+    unzip_dir: Path
